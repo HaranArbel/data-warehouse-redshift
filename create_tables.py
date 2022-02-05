cb3882ba16f4eb6f06737566d087dbb9c9d41d1e
Haran Arbel <haranarbel@gmail.com>
2022-02-05 18:04:36 +0200

    added documentation

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,18 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    this drops the database tables if exist
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    this creates the staging, fact and dimension tables
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
